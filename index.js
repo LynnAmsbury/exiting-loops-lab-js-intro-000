0cf90778473deff9190c644c4f89c3a7db7f079c
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-18 03:04:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,9 +3,24 @@ function breakOut(array, changeValue, stopValue) {
     if (array[i] === stopValue) {
       break;
     }
-
     array[i] = changeValue;
   }
-
   return array;
+}
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === skipValue) {
+      continue
+    }
+    array[i]++;
+  }
+}
+
+for (let i = 0; i < scores.length; i++) {
+  if (scores[i] >= 10) {
+    continue
+  }
+
+  scores[i]++
 }
