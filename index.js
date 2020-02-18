b634f5fdcd89a00570e91309d1e87cda0b77fa6b
Lynn Amsbury <43070638+lynnamsbury@users.noreply.github.com>
2020-02-18 03:10:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,6 +20,10 @@ function keepGoing(array, changeValue, skipValue) {
 
 function findBy(array, findFn) {
   for (let i = 0; i < array.length; i++) {
-    
+    if (array[i] === findFn()) {
+      return array;
+    } else {
+      return null;
+    }
   }
 }
